b3208c58ca727921f1955831370f9011e1ef97df
vansh9636 <154052495+vansh9636@users.noreply.github.com>
2024-07-14 20:05:18 +0530

    Add files via upload

---
+++ Day-1.js
@@ -0,0 +1,64 @@
+// day-1: variable and data type
+
+// variable Declaration
+
+//Declare a variable using var,let
+var a="hellow world !";
+console.log(a);
+
+// variable using let
+
+let b = "JavaScript";
+console.log(b);
+
+// Constant Declaration
+
+const c = "constant";
+console.log(c);
+
+// assign  an initial value in let variable , reassign a new value 
+
+let myname="vansh";
+myname="vassu";
+console.log(myname);
+
+//try with another data type
+
+let fNum=15;
+fNum=25;
+console.log(fNum);
+
+// we not reassign a value with the const variable.
+
+const val="jhunjhunu";
+val="sikar"; // here is no change the value of val,that is a constant
+
+
+
+// ..... Data Types  ....
+
+let str=" hellow world"// string 
+console.log(str);
+
+let num=45; // number
+console.log(num); 
+
+let bool = true; // boolean 
+console.log(bool);
+
+let dets={ name :" vansh kumawat " , address:" jhunjhunu"};// object 
+console.log( dets );
+
+let arr=[1,2,3,4];
+console.log(arr);
+
+let  not = null; // null , for null value 
+console.log(not)
+
+// we use " typeof" operator for check data type of any value
+
+console.log( typeof num);
+
+
+
+
